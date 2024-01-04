154d39a27c689e864fde19f7dbdeadf976dbc234
Herradon <73962361+Herradon@users.noreply.github.com>
2024-01-04 04:28:20 +0100

    casi fin de web

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
-//BOTON ALERTA 
+// Variables
 
-//FINAL BOTON ALERTA
+const collapseElementList = document.querySelectorAll('.collapse')
+const collapseList = [...collapseElementList].map(collapseEl => new bootstrap.Collapse(collapseEl))
+
+//pago
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('en breves le enviaremos la factura a su correo', 'success')
+  })
+}
+
