d8f71830ebddebea629f3a678f56ffc05b8a7d32
Herradon <73962361+Herradon@users.noreply.github.com>
2024-05-08 20:16:01 +0200

    cambios rapidos

--- js/script.js
+++ js/script.js
@@ -134,3 +134,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // on click 
 
+document.querySelector('#ebtity').object3D.lookAt('#camera');
+
+AFRAME.registerComponent('look-at', {
+  schema: { type: 'selector' },
+
+  init: function () {},
+
+  tick: function () {
+    this.el.object3D.lookAt(this.data.object3D.position)
+  }
+})
