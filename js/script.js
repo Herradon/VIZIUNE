111710ee9c077c86aaabddcf120a916e5f1019e8
Herradon <73962361+Herradon@users.noreply.github.com>
2024-05-10 16:44:01 +0200

    inicio de sesión y registro

--- js/script.js
+++ js/script.js
@@ -145,3 +145,24 @@ AFRAME.registerComponent('look-at', {
     this.el.object3D.lookAt(this.data.object3D.position)
   }
 })
+
+// COMIENZO DE AJUSTE DE NUEVO MODAL 
+
+const exampleModal = document.getElementById('registroModal')
+if (exampleModal) {
+  exampleModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-whatever')
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalTitle = exampleModal.querySelector('.modal-title')
+    const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+    modalTitle.textContent = `New message to ${recipient}`
+    modalBodyInput.value = recipient
+  })
+}
